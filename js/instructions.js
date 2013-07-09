e1544ef1d95d8fc91cbe4994181d51d761cb96c5
Andrew Hyder <hyd415@gmail.com>
2013-07-09 14:08:41 -0700

    Cleaned up how the step transitions worked.

--- js/instructions.js
+++ js/instructions.js
@@ -4,10 +4,10 @@ var instructions = (function (instructions) {
   var debug = true;
   var bodyPadding = 0;
   var lessonId = 0; // Blank lesson
-  var lesson = null;
-  var steps = null;
+  var lesson = {};
+  var steps = [];
   var accessToken = null;
-  var currentStep = 0;
+  var currentStep = 1;
   var checkStepInterval;
   var htcApiUrl = null;
   var htcApiVer = null;
@@ -33,9 +33,8 @@ var instructions = (function (instructions) {
 
   // Fill in the blanks with the lesson
   // sets up post-json load initialization
-  function _jsonLoadSuccess(lessonResponse)
+  function _jsonLoadSuccess(lesson)
   {
-    lesson = lessonResponse;
     // Make sure steps are in order of id
     steps = lesson.steps.sort(function(a, b){
       if (a.id < b.id) return -1;
@@ -48,14 +47,14 @@ var instructions = (function (instructions) {
 
     // Loop through steps and fill out the page
     $(steps).each(function(i){
-      $('#main').append('<section id="'+lesson.steps[i].url+'">'+
+      $('#main').append('<section id="'+steps[i].url+'">'+
               ' <h2 class="step_name"></h2>'+
               ' <div class="step_text"></div>'+
               ' <div class="feedback"></div>'+
               '</section>')
-      $('#'+lesson.steps[i].url+' .step_name').html(lesson.steps[i].name);
-      $('#'+lesson.steps[i].url+' .step_text').html(lesson.steps[i].step_text);
-      $('#'+lesson.steps[i].url+' .feedback').html(lesson.steps[i].feedback);
+      $('#'+steps[i].url+' .step_name').html(steps[i].name);
+      $('#'+steps[i].url+' .step_text').html(steps[i].step_text);
+      $('#'+steps[i].url+' .feedback').html(steps[i].feedback);
     })
 
     // OAuth
@@ -79,37 +78,53 @@ var instructions = (function (instructions) {
   // next button is clicked
   function _nextClicked(evt)
   {
-    if (debug) console.log('Next');
-    var next_step = currentStep + 1;
-    $('html, body').animate({ scrollTop: $('#step'+next_step).offset().top - bodyPadding }, 1000);
-  }
+    if (currentStep < steps.length){
+      currentStep = currentStep + 1;
+      $('html, body').animate({ scrollTop: $('#step'+currentStep).offset().top - bodyPadding }, 1000);
+    }}
 
   // back button is clicked
   function _backClicked(evt)
   {
-    if (debug) console.log('Back');
-    var backStep = currentStep;
-    $('html, body').animate({ scrollTop: $('#step'+backStep).offset().top - bodyPadding }, 1000);
+    if (currentStep > 1){
+      currentStep = currentStep - 1;
+      $('html, body').animate({ scrollTop: $('#step'+currentStep).offset().top - bodyPadding }, 1000);
+    }
   }
 
   function _checkStep(){
+
+      // Make object of the current stop
+      step = {
+        id : steps[currentStep - 1].id,
+        name : steps[currentStep - 1].name,
+        description : steps[currentStep - 1].description,
+        url : steps[currentStep - 1].url,
+        stepText : steps[currentStep - 1].step_text,
+        lessonId : steps[currentStep - 1].lesson_id,
+        triggerEndpoint : steps[currentStep - 1].trigger_endpoint,
+        triggerCheck : steps[currentStep - 1].trigger_check,
+        triggerValue : steps[currentStep - 1].trigger_value,
+        nextStep : steps[currentStep - 1].next_step
+      }
+
       var trigger = false;
       if (accessToken != null) {
         // Need to add in some debug info if these don't exist.
-        if (steps[currentStep].trigger_endpoint != '' && 
-            steps[currentStep].trigger_check != '' && 
-            steps[currentStep].trigger_value != ''){
+        if (step.triggerEndpoint != '' && 
+            step.triggerCheck != '' && 
+            step.triggerValue != ''){
                     
           // If step type is login
-          if (steps[currentStep].description == 'login'){
-            // An example trigger_endpoint
+          if (step.description == 'login'){
+            // An example triggerEndpoint
             // https://api.foursquare.com/v2/users/self?v=20130706&oauth_token=
-            $.getJSON(steps[currentStep].trigger_endpoint+accessToken, _loginJsonLoaded)
+            $.getJSON(step.triggerEndpoint+accessToken, _loginJsonLoaded)
           }
         }
 
         // If step type is open
-        if (steps[currentStep].description == 'open'){
+        if (step.description == 'open'){
           $("#open").click(_openClicked);
         }
       }
@@ -118,36 +133,32 @@ var instructions = (function (instructions) {
     // #open is clicked
     function _openClicked(evt)
     {
-      var height = window.screen.height;
       var challengeFeatures = {
-        height: height,
+        height: window.screen.height,
         width: 1000,
         name: 'challengeWindow',
         center: false
       }
-      challengeWindow = $.popupWindow(steps[currentStep].trigger_endpoint, challengeFeatures);
-      $('html, body').delay(3000).animate({ scrollTop: $('#'+steps[currentStep].next_step).offset().top - bodyPadding }, 1000);
-      $('#'+steps[currentStep].url+' .feedback').css('display','block');
-      currentStep = currentStep + 1;
+      challengeWindow = $.popupWindow(step.triggerEndpoint, challengeFeatures);
+      // $('html, body').delay(3000).animate({ scrollTop: $('#'+steps[currentStep - 1].nextStep).offset().top - bodyPadding }, 1000);
+      $('#'+step.url+' .feedback').css('display','block');
     }
 
     // step type login json has loaded
     function _loginJsonLoaded(response)
     {
+      var trigger = false;
       // Cast strings to booleans
-      var triggerValue = steps[currentStep].trigger_value;
-      if (triggerValue == 'true') triggerValue = true;
-      if (triggerValue == 'false') trigger_Value = false;
+      if (step.triggerValue == 'true') step.triggerValue = true;
+      if (step.triggerValue == 'false') step.triggerValue = false;
 
       // Check the trigger vs the value to see if its correct
-      if (eval(steps[currentStep].trigger_check) == triggerValue){
+      if (eval(step.triggerCheck) == step.triggerValue){
         trigger = true;
       }
       if (trigger == true){
-        console.log(steps[currentStep].next_step);
-        $('html, body').delay(3000).animate({ scrollTop: $('#'+steps[currentStep].next_step).offset().top - bodyPadding }, 1000);
-        $('#'+lesson.steps[currentStep].url+' .feedback').css('display','block');
-        currentStep = currentStep + 1;
+        // $('html, body').delay(3000).animate({ scrollTop: $('#'+steps[currentStep - 1].nextStep).offset().top - bodyPadding }, 1000);
+        $('#'+step.url+' .feedback').css('display','block');
       }
     }
 
