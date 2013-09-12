ac739f6520c131e545ee079d66b6ba80491c39ef
Andrew Hyder <hyd415@gmail.com>
2013-09-12 12:15:41 -0700

    Correct js config for release.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES
 var config = {
   debug : false,
-  bfUrl : 'https://howtocity.herokuapp.com'
+  bfUrl : 'https://howtocity.herokuapp.com',
   // bfUrl : 'https://howtocity-staging.herokuapp.com',
   // bfUrl : 'http://127.0.0.1:8000',
   bfApiVersion : '/api/v1'
