f65bb4a5ff5749179f1d1551147149da7c40e8cc
Andrew Hyder <hyd415@gmail.com>
2013-10-06 18:01:23 -0700

    Config for staging.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // GLOBAL VARIABLES
 var config = {
   debug : true,
-  // bfUrl : 'https://howtocity-staging.herokuapp.com',
-  bfUrl : 'http://127.0.0.1:8000',
+  bfUrl : 'https://howtocity-staging.herokuapp.com',
+  // bfUrl : 'http://127.0.0.1:8000',
   // bfUrl : 'http://0.0.0.0:5000',
   bfApiVersion : '/api/v1'
 }
