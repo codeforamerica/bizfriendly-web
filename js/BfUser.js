47ecfffc6da6bfec3c5f82076e9692d48b08ef6c
Andrew Hyder <hyd415@gmail.com>
2013-10-24 11:46:48 -0700

    Debug request password reset."

--- js/BfUser.js
+++ js/BfUser.js
@@ -144,7 +144,7 @@ var BfUser = (function (BfUser)  {
   function _requestPasswordResetClicked(response){
     var email = {"email" : $('#reset-email').val()}
     $.post(config.bfUrl+'/request_password_reset',email, function(response){
-      if (config.debug) console.log(response);
+      console.log(response);
       response = $.parseJSON(response);
       console.log(response.message);
       if (response.message == "Queued. Thank you."){
