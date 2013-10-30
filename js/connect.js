50578168297eb4721cb58ad7ac272b08a985141a
Andrew Hyder <hyd415@gmail.com>
2013-10-29 18:06:49 -0700

    Connect page good.

--- js/connect.js
+++ js/connect.js
@@ -21,20 +21,13 @@ var connect = (function (connect) {
   function _main(response){
     $('#loading').toggle();
     $('#main').toggle();
-    // if (config.debug) console.log(response);
     _checkIfLoggedIn();
-
-    $.each(response.objects, function(i){
-      // console.log(response.objects[i]);
-      if (response.objects[i].end_dt){
-        console.log(response.objects[i]);
-        var html = '<p><a>'+response.objects[i].user.name+'</a>';
-        html += " recently finished ";
-        html += '<a href="service.html?'+response.objects[i].lesson.service_id+'">'+response.objects[i].lesson.name+'</a></p>';
-        html += '<hr/>';
-        $("#recent-activity").append(html);
-      }
-    })
+    // Get top learners
+    _getTopLearners(response);
+    // Get Top Teachers
+    _getTopTeachers(response);
+    // Get latest activity
+    _getLatestActivity(response);
   }
 
   function _checkIfLoggedIn(){
@@ -48,6 +41,122 @@ var connect = (function (connect) {
       // if (config.debug) console.log('Logged In');
       $(".user-name").text(BfUser.name);
     }
+  }
+
+  function _getTopLearners(response){
+    var userCounts = {};
+    var userNames = {};
+    // Build an object of user ids and counts of lessons completed.
+    $.each(response.objects, function(i,userLesson){
+      // If user isn't in the list yet, add them.
+      if (!userCounts[userLesson.user.id]) {
+        userCounts[userLesson.user.id] = 0;
+      }
+      // If they've finished a lesson, then plus one to the count.
+      if (userLesson.end_dt){
+        userCounts[userLesson.user.id] += 1;
+      }
+      // Build an object of user ids and names
+      if(!userNames[userLesson.user.id]){
+        userNames[userLesson.user.id] = userLesson.user.name;
+      }
+    });
+    // Match names to the userids
+    var namedCounts = []
+    $.each(userCounts,function(userId,completedLessonsCount){
+      var nameCount = {}
+      nameCount["name"] = userNames[userId];
+      nameCount["count"] = userCounts[userId];
+      namedCounts.push(nameCount);
+    })
+    // Sort
+    namedCounts.sort(function(a,b){
+      if (a.count > b.count) return -1;
+      if (a.count < b.count) return 1;
+      return 0;
+    })
+    // Add to page
+    var html = "";
+    $.each(namedCounts, function(i,namedCount){
+      if (i < 5){ // Top five learners
+        html += '<p><a>'+namedCount.name+'</a>';
+        html += " has taken "+namedCount.count+" lessons.</p>";
+        // html += '<hr/>';
+      }
+    })
+    // Add to the page
+    $("#top-learners").append(html);
+  }
+
+  function _getTopTeachers(response){
+    teacherCounts = {};
+    $.each(response.objects, function(i,userLesson){
+      // If teacher isn't in the object, add it
+      if (!teacherCounts[userLesson.lesson.creator_id]){
+        teacherCounts[userLesson.lesson.creator_id] = 0;
+      }
+      if (userLesson.end_dt){
+        teacherCounts[userLesson.lesson.creator_id] += 1
+      }
+    })
+    console.log(teacherCounts);
+    // Get names of teachers
+    // var html = ""
+    var namedCounts = [];
+
+    $.each(teacherCounts, function(id,count){
+      console.log(id, count);
+      var namedCount = {}
+      $.getJSON(config.bfUrl+config.bfApiVersion+'/users/'+id, function(response){
+        namedCount["name"] = response.name;
+        namedCount["count"] = count;
+        namedCounts.push(namedCount)
+        // Sort
+        namedCounts.sort(function(a,b){
+          if (a.count > b.count) return -1;
+          if (a.count < b.count) return 1;
+          return 0;
+        })
+        // Add to the page
+        console.log(namedCounts);
+        var html = "";
+        $.each(namedCounts, function(i,namedCount){
+          if (i < 5){ // Top five learners
+            html += '<p><a>'+namedCount.name+'</a>';
+            html += " has taught "+namedCount.count+" lessons.</p>";
+            // html += '<hr/>';
+          }
+        })
+        // Add to the page
+        $("#top-teachers").append(html);
+      })
+    })
+  }
+
+  function _getLatestActivity(response){
+    var recentlyCompletedLessons = []
+    // Get finished lessons
+    $.each(response.objects, function(i,userLesson){
+      if (userLesson.end_dt){
+        recentlyCompletedLessons.push(userLesson);
+      }
+    });
+    // Sort by time
+    recentlyCompletedLessons.sort(function(a,b){
+      if (a.end_dt > b.end_dt) return -1;
+      if (a.end_dt < b.end_dt) return 1;
+      return 0;
+    })
+    // Add to page
+    var html = ""
+    $.each(recentlyCompletedLessons, function(i,userLesson){
+      html += '<p><a>'+userLesson.user.name+'</a>';
+      html += " recently finished ";
+      html += '<a href="service.html?'+userLesson.lesson.service_id+'">'+userLesson.lesson.name+'</a></p>';
+      // html += '<hr/>';
+    })
+    // Add to the page
+    $("#latest-activity").append(html);
   }
 
   // add public methods to the returned module and return it
