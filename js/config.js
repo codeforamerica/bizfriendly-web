d966ba35aa7d24a23622bdf926322ba10c15afc5
Andrew Hyder <hyd415@gmail.com>
2013-11-03 21:24:16 -0600

    Production config.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES
 var config = {
-	debug : true,
-  // debug : false,
+	// debug : true,
+  debug : false,
   bfUrl : 'https://howtocity.herokuapp.com',
   // bfUrl : 'https://howtocity-staging.herokuapp.com',
   // bfUrl : 'http://127.0.0.1:8000',
