e16e137a40734acee211511d61ac572ccc220464
Andrew Hyder <hyd415@gmail.com>
2013-11-03 20:54:14 -0600

    Admin can edit others lessons in the lesson-builer now.

--- js/teach.js
+++ js/teach.js
@@ -69,11 +69,15 @@ var teach = (function (teach) {
 
   function _getExistingLessonData(){
     $.getJSON(config.bfUrl+config.bfApiVersion+'/lessons/'+lessonId, function(lesson){
-       // Check for owner or admin
+      // Check for owner or admin
       if(BfUser.id != lesson.creator.id){
-        $('#teach-main').hide();
-        $(".login-required").text("Whoa, that is someone elses lesson.");
-        $('.login-required').show();
+        $.getJSON(config.bfUrl+'/'+BfUser.id+'/is_admin', function(isAdmin){
+          if (!isAdmin.response){
+            $('#teach-main').hide();
+            $(".login-required").text("Whoa, that is someone elses lesson.");
+            $('.login-required').show();
+          }
+        })
       }
 
       // console.log(lesson);
