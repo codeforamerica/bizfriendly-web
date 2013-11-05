57b456a2546d8bfbcf411dfe3a5959012e3b05e9
Andrew Hyder <hyd415@gmail.com>
2013-11-05 12:15:08 -0600

    Alert for IE9 bugs.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
 var config = {
-  debug : true,
+  debug : false,
   bfUrl : 'https://howtocity.herokuapp.com',
   // bfUrl : 'https://howtocity-staging.herokuapp.com',
   // bfUrl : 'http://127.0.0.1:8000',
