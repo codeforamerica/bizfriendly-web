13f6f3132c0b3c671c1e9100af1e606bd101c356
Andrew Hyder <hyd415@gmail.com>
2014-02-04 08:37:30 -0500

    Set up for staging.

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ var config = {
   debug : true,
   // bfUrl : 'http://app.bizfriend.ly',
   // bfUrl : 'http://howtocity.herokuapp.com',
-  // bfUrl : 'http://app-staging.bizfriend.ly',
-  bfUrl : 'http://127.0.0.1:8000',
+  bfUrl : 'http://app-staging.bizfriend.ly',
+  // bfUrl : 'http://127.0.0.1:8000',
   // bfUrl : 'http://0.0.0.0:5000',
   bfApiVersion : '/api/v1'
 }
