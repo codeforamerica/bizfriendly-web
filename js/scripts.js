3774601540376cb4361af22595995828311faea6
Andrew Hyder <hyd415@gmail.com>
2014-10-13 17:33:51 -0700

    The rest of the subpages. Closes #63

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,11 @@
 	          	$(this).addClass("active");
           	}
 
-          	if (pgurl == "teaching-guide.html" || pgurl == "content-requests.html")
+			if (pgurl == "teaching-guide.html" || pgurl == "content-requests.html" || "new-category.html" || "new-skill.html" || "lesson-builder.html") {
 				$("#teach-header-nav").addClass("active");
+			}
+			if (pgurl.indexOf("service.html") != -1) {
+				$("#learn-header-nav").addClass("active");
+			}
 	     })
 	});
